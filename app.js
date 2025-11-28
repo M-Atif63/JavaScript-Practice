0cf5d650a231835460d4b56c7226c2e1192db128
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2025-11-29 04:15:40 +0500

    Practice of js chapter #6(six) math expressions unfamiliar operators

---
+++ app.js
@@ -0,0 +1,45 @@
+// ====================Practice Of=====================
+// ================ Chapter No 6 (Six) =================
+// ===== Math Expressions Unfamiliar Operators ===========
+
+// var a = 1;
+
+// var b = 1;
+
+// var total = ++a+ b++ + a++ + ++a + ++b;
+// //             2 +   1   +  2    +   4   +  3
+// console.log(total);
+
+// var total = (a++) + (++a) + (++b) + (a++) + (b++) + (a++) + (++b);
+// //            1   +    3  +    2  +   3   +   2   +   4   +  4 = 18
+
+// console.log(total);
+
+// var a = 2;
+// var b = 3;
+// var c = 4;
+// var d = 5;
+
+// var result = (a++) + (++b) + (++a) + (++b) + (++c) + (d++) + (c++) + (++d) + (a++) + (++d) + (++a);
+// //              2  +   4 +      4  +    5  +    5  +    5  +   5   +    7  +   4   +   8  +    6
+
+// console.log(result)
+
+// var a = 2;
+// var b = 3;
+// var c = 4;
+// var d = 5;
+
+// var total = d++ + ++a + ++b + c++ + ++a + ++d + a++ + d++ + ++c + b++;
+// //           5  +  3  +  4  +  4  +  4  +  7  +  4  +  7  +  6  +  4;
+
+// console.log(total)
+
+// var x = 12;
+// var y = 21;
+// var z = 31;
+
+// var result = y++ + ++x + ++y + z++ + ++y + x++ + ++z + y++ + x++ + ++y + ++x;
+// //            21    13 +  23 +  31 +  24 +  13 +  33 +  24 + 14  + 26  +  16
+
+// console.log(result);
