fb1734021ec0ee234436e4612a23de9bf21beb11
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2026-07-11 07:13:57 +0500

    feat: Remove login form and associated JavaScript functionality

--- login.js
+++ login.js
@@ -1,46 +1,71 @@
-console.log("login is live")
+// console.log("login is live")
 
 
-let usersData = [
-    {
-        name:'ali',
-        eml:'ali@gmail.com',
-        pass:12345
-    },
-    {
-        name:'ahmed',
-        eml:'ahmed@gmail.com',
-        pass:12345
-    },
-    {
-        name:'bilal',
-        eml:'bilal@gmail.com',
-        pass:12345
-    },
-    {
-        name:'atif',
-        eml:'atif@gmail.com',
-        pass:12345
-    },
+// let usersData = [
+//     {
+//         name:'ali',
+//         eml:'ali@gmail.com',
+//         pass:12345
+//     },
+//     {
+//         name:'ahmed',
+//         eml:'ahmed@gmail.com',
+//         pass:12345
+//     },
+//     {
+//         name:'bilal',
+//         eml:'bilal@gmail.com',
+//         pass:12345
+//     },
+//     {
+//         name:'atif',
+//         eml:'atif@gmail.com',
+//         pass:12345
+//     },
     
-]
+// ]
 
-let userName = document.getElementById('username')
-let email = document.getElementById('email')
-let password = document.getElementById('password')
 
-userName.addEventListener('change',(e)=>{
-    console.log("=>",e.target.value)
-})
+// let myPromise = new Promise((resolve,reject)=>{
+//     console.log("data is fetching")
+//     let prom = false;
+//     if(prom){
+//         resolve("successful")
+//     }else{
+//         reject("failed")
+//     }
+// });
 
-for(const key in usersData){
-    let submitBtn = document.getElementById('submitBtn')
-    submitBtn.addEventListener('click',()=>{
-        // if(userName.value === usersData[key].name && email === usersData[key].eml && password === usersData[key].pass){
-        // console.log('user is here')
-        // }
-        // if(userName === usersData[key].name){
-        //     console.log("name is here")
-        // }
-    })
-}
+// let onFilfullation = (result) =>{
+//     console.log(result)
+// }
+// let onRejection = (error) =>{
+//     console.log(error)
+// }
+// myPromise.then(
+//     function onFilfullation(result){
+//         console.log(result)
+//     }
+// ).catch(
+//     function onRejection(error){
+//         console.log(error)
+//     }
+// )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
